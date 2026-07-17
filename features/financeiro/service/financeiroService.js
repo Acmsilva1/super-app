aa7e4ed419f53a597314e55fe0be9db9534a1268
Acmsilva1 <andrecarlos.miranda@gmail.com>
2026-07-17 17:52:16 -0300

    Clarify financeiro purchases label

--- features/financeiro/service/financeiroService.js
+++ features/financeiro/service/financeiroService.js
@@ -53,7 +53,7 @@ export function canonicalFinanceiroCategoriaLabel(value) {
     lazer: 'Lazer',
     saude: 'Sa\u00fade',
     ticket: 'Ticket',
-    compras: 'Compras',
+    compras: 'Eletro/moveis',
     contas: 'Contas',
     outros: 'Outros',
     outro: 'Outro',
